329f9564fa2cefece5c04c2de37b3ae0fbd26f37
GiovanniCadoni <giocadoni19@gmail.com>
2023-12-13 17:54:41 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iscrizione Newsletter</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container py-4 w-25">
+        <h1 class="fw-bolder">Iscriviti alle nostre newsletter</h1>
+        <form class="py-4">
+            <label for="userEmail" class="form-label">Email address:</label>
+            <input type="text" class="form-control" id="userEmail" name="userEmail">
+            <button type="submit" class="btn btn-primary my-4">Verify</button>
+        </form>
+    </div>
+</body>
+
+</html>
